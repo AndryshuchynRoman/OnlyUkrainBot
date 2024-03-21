a7d2c1392e8d5ff52b6e73996b2a655ecba24919
Tim0n4ik <116250152+Tim0n4ik@users.noreply.github.com>
2024-03-21 15:51:21 +0200

    Init commit

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
+from config import telegram_bot_token
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
+
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('TOKEN').build()
+    application = ApplicationBuilder().token(telegram_bot_token).build()
 
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
