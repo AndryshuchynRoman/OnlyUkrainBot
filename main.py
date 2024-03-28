152c523e37c899324a0cf7a4779ea51f03aaf71f
Tim0n4ik <116250152+Tim0n4ik@users.noreply.github.com>
2024-03-28 15:06:39 +0200

    implemented simple bot

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
+import os
 import logging
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
-from config import telegram_bot_token
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes,MessageHandler, filters
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
+async def hello (update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'хаю хай {update.effective_user.first_name}')
+async def HI(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'віііііііі {update.effective_user.first_name}')
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+   messege = update.message.text.lower()
 
-if __name__ == '__main__':
-    application = ApplicationBuilder().token(telegram_bot_token).build()
+   if 'привіт' in messege:
+       reply_text = f'привіт {update.effective_user.first_name}!'
+   else:
+       reply_text = 'Я тебе не розумію '
 
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
+   await update.message.reply_text(reply_text)
+app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
-    application.run_polling()
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("hello", hello))
+app.add_handler(CommandHandler("HI", HI))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+app.run_polling()
